d0ec27bfb59cdeb739c2443d7f42491ccf54b6de
MelonRice <lihongwei850718@gmail.com>
2018-07-02 16:27:29 +0800

    move code

---
+++ lib/exampleDemo/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:starter/exampleDemo/sampleApp2.dart';
+import 'package:starter/exampleDemo/sampleApp1.dart';
+import 'package:starter/exampleDemo/fadeAppTest.dart';
+import 'package:starter/exampleDemo/navigatorTest.dart';
+import 'package:starter/exampleDemo/platformChannel.dart';
+import 'package:starter/zhihu/zhihudaily.dart';
+import 'package:starter/exampleDemo/showLoading.dart';
+import 'package:starter/exampleDemo/isolateApp.dart';
+import 'package:starter/exampleDemo/listItem.dart';
+
+void main() => runApp(new ZhihuDailyApp());
